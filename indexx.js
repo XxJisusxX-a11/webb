f85fc71db500de3ad236dd170753abe55b2c8bd3
XxJisusxX-a11 <mongejesus263@gmail.com>
2024-10-26 00:25:41 -0500

    Add files via upload

---
+++ indexx.js
@@ -0,0 +1,14 @@
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slide');
+
+document.getElementById('next').addEventListener('click', () => {
+    slides[currentSlide].classList.remove('active');
+    currentSlide = (currentSlide + 1) % slides.length;
+    slides[currentSlide].classList.add('active');
+});
+
+document.getElementById('prev').addEventListener('click', () => {
+    slides[currentSlide].classList.remove('active');
+    currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+    slides[currentSlide].classList.add('active');
+});
